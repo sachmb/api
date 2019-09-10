e7a22d53c81002996b96d290d9aaf34dbffc9475
unknown <sachham3@gmail.com>
2019-09-10 14:31:34 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const db = mongoose.connect('mongodb://localhost/bookAPI');
+const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/api', bookRouter);
+
+app.get('/', (req, res) => {
+  res.send('welcome to my Nodemon API!');
+});
+
+app.listen(port, () => {
+  console.log(`running on port ${port}`);
+});
